61f59223a95d061d20b395cb5a059ff49d007ed8
James Pond <james.pond@datadoghq.com>
2024-05-17 14:43:34 -0300

    Add new internal package for custom errors
    Signed-off-by: James Pond <james.pond@datadoghq.com>

---
+++ internal/errno/errno.go
@@ -0,0 +1,69 @@
+// Package errno provides a way to handle errors with exit codes.
+package errno
+
+// Exit codes for errors.
+const (
+	// ExitOK is the exit code for a successful operation.
+	ExitOK uint8 = iota
+
+	// ExitError is the exit code for a generic error.
+	ExitError
+
+	// ExitUsage is the exit code for an incorrect usage of a command.
+	ExitUsage
+
+	// ExitIO is the exit code for an I/O error.
+	ExitIO
+
+	// ExitNotFound is the exit code for a file not found error.
+	ExitNotFound
+
+	// ExitPermission is the exit code for an I/O permission denied error.
+	ExitPermission
+
+	// ExitAPIError is the exit code for errors related to API requests.
+	ExitAPIError
+
+	// ExitUnauthorized is the exit code for unauthorized requests to the OpenAI
+	// API.
+	ExitUnauthorized
+
+	// ExitTimeout is the exit code for timeout errors.
+	ExitTimeout
+)
+
+// Error is an error with an exit code.
+type Error struct {
+	// Err is the underlying error that caused this error.
+	Err error
+
+	// No is the machine-readable exit code.
+	No uint8
+}
+
+// Error implements the error interface for Error.
+func (e *Error) Error() string {
+	if e.Err != nil {
+		return e.Err.Error()
+	}
+
+	return "unknown error"
+}
+
+// Unwrap returns the underlying error.
+func (e *Error) Unwrap() error {
+	return e.Err
+}
+
+// Code returns the exit code of the error as an integer.
+func (e *Error) Code() int {
+	return int(e.No)
+}
+
+// New creates a new Error with the given exit code and error.
+func New(code uint8, err error) *Error {
+	return &Error{
+		No:  code,
+		Err: err,
+	}
+}
---
+++ internal/errno/errno_test.go
@@ -0,0 +1,163 @@
+package errno_test
+
+import (
+	"errors"
+	"testing"
+
+	"git.sr.ht/~jamesponddotco/xstd-go/xerrors"
+	"github.com/DataDog/documentor/internal/errno"
+)
+
+const ErrGenericError xerrors.Error = "failure"
+
+func TestError_Error(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name string
+		err  error
+		want string
+	}{
+		{
+			name: "With underlying error",
+			err:  ErrGenericError,
+			want: "failure",
+		},
+		{
+			name: "With nil underlying error",
+			err:  nil,
+			want: "unknown error",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			err := errno.Error{
+				Err: tt.err,
+			}
+
+			if got := err.Error(); got != tt.want {
+				t.Errorf("Error.Error() = %q, want %q", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestError_Unwrap(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name string
+		err  error
+		want error
+	}{
+		{
+			name: "With underlying error",
+			err:  ErrGenericError,
+			want: ErrGenericError,
+		},
+		{
+			name: "With nil underlying error",
+			err:  nil,
+			want: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			err := errno.Error{
+				Err: tt.err,
+			}
+
+			if got := err.Unwrap(); !errors.Is(got, tt.want) {
+				t.Errorf("Error.Unwrap() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestError_Code(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name string
+		no   uint8
+		want int
+	}{
+		{
+			name: "ExitOK",
+			no:   errno.ExitOK,
+			want: 0,
+		},
+		{
+			name: "ExitError",
+			no:   errno.ExitError,
+			want: 1,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			err := errno.Error{
+				No: tt.no,
+			}
+
+			if got := err.Code(); got != tt.want {
+				t.Errorf("Error.Code() = %d, want %d", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNew(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		code     uint8
+		err      error
+		wantCode int
+		wantErr  error
+	}{
+		{
+			name:     "With nil error",
+			code:     errno.ExitError,
+			err:      nil,
+			wantCode: 1,
+			wantErr:  nil,
+		},
+		{
+			name:     "With non-nil error",
+			code:     errno.ExitError,
+			err:      ErrGenericError,
+			wantCode: 1,
+			wantErr:  ErrGenericError,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := errno.New(tt.code, tt.err)
+
+			if got.Code() != tt.wantCode || !errors.Is(got.Unwrap(), tt.wantErr) {
+				t.Errorf("New(%d, %v) = {Code: %d, Err: %v}, want {Code: %d, Err: %v}", tt.code, tt.err, got.Code(), got.Unwrap(), tt.wantCode, tt.wantErr)
+			}
+		})
+	}
+}
